36c90b19c525828427c74de2bf44342011452405
NoReply Amazon <no-reply@amazon.com>
2025-02-20 18:11:32 +0000

    [Commit] Iteration 8 #errors = 56 <= 68.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryCreateUpdateViewModel.cs][CS0246][53,28] The type or namespace name 'SelectListItem' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryCreateUpdateViewModel.cs
+++ app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryCreateUpdateViewModel.cs
@@ -7,6 +7,7 @@ using Bookstore.Domain.Books;
 using Bookstore.Domain.ReferenceData;
 using Bookstore.Web.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 namespace Bookstore.Web.Areas.Admin.Models.Inventory
