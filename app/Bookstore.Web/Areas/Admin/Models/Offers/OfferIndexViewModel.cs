665b7783d72bdccf39099831f7393da705473d0a
NoReply Amazon <no-reply@amazon.com>
2025-02-20 18:13:20 +0000

    [Commit] Iteration 11 #errors = 41 <= 47.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Areas/Admin/Models/Offers/OfferIndexViewModel.cs][CS0246][47,28] The type or namespace name 'SelectListItem' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Areas/Admin/Models/Offers/OfferIndexViewModel.cs
+++ app/Bookstore.Web/Areas/Admin/Models/Offers/OfferIndexViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 namespace Bookstore.Web.Areas.Admin.Models.Offers
