6fea772adea0deacb8ce1dc0ea70ef40f3f660f7
awstransform <ecs-user@ip-172-31-66-186.ec2.internal>
2025-11-10 14:41:43 +0000

    AWS Transform commit for jobId 2ace3c02-8295-44a1-9b03-aa498f3a3d1f

--- app/Bookstore.Web/Areas/Admin/AdminAreaRegistration.cs
+++ app/Bookstore.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -1,23 +1,25 @@
-﻿using System.Web.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
 
 namespace Bookstore.Web.Areas
 {
-    public class AdminAreaRegistration : AreaRegistration 
+    public static class AdminAreaRegistration
     {
-        public override string AreaName 
+        public static string AreaName
         {
-            get 
+            get
             {
                 return "Admin";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public static void RegisterArea(IEndpointRouteBuilder endpoints)
         {
-            context.MapRoute(
-                "Admin_default",
-                "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }, namespaces: new[] { "Bookstore.Web.Areas.Admin.Controllers" }
+            endpoints.MapAreaControllerRoute(
+                name: "Admin_default",
+                areaName: "Admin",
+                pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
             );
         }
     }
